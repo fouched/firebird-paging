c7c5ec5fd2624a7c61c5a53da31d4916a27c709b
Fouche du Preez <fouche@limehouse.co.za>
2022-03-11 08:39:03 +0200

    init

---
+++ src/main/java/za/co/limehouse/firebird/paging/FirebirdPagingApplication.java
@@ -0,0 +1,13 @@
+package za.co.limehouse.firebird.paging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebirdPagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebirdPagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/limehouse/firebird/paging/FirebirdPagingApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.limehouse.firebird.paging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebirdPagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
